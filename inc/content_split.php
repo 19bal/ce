c0c6780708900e8e640a742a42ff421dd96debf0
yunus <yunus.ates@bil.omu.edu.tr>
2011-12-09 21:11:32 +0200

    Update inc/content_split.php

--- inc/content_split.php
+++ inc/content_split.php
@@ -8,45 +8,49 @@ function content_split($s) {
 			'İlaç Etkileşimleri:',
 			'Doz Önerisi:');
 
-	$dict = array();
+		$siralanacak = array();
 
-	if (preg_match("/Doz Önerisi/", $s))
+	if (preg_match("/Doz Önerisi/", $s ,$match1, PREG_OFFSET_CAPTURE))
 	{
-		$k = explode($wanted[6], $s);
-		$dict[$wanted[6]] = $k[1];
-		$s = $k[0];
+		array_push($siralanacak,$match1[0][1]);		
 	}
-	if (preg_match("/İlaç Etkileşimleri/", $s))
+	if (preg_match("/İlaç Etkileşimleri/", $s, $match2, PREG_OFFSET_CAPTURE))
 	{
-		$m = explode($wanted[5], $s);
-		$dict[$wanted[5]] = $m[1];
-		$s = $m[0];
+		array_push($siralanacak, $match2[0][1]);
 	}
-	if (preg_match("/Yan Etkileri/", $s))
+	if (preg_match("/Yan Etkileri/", $s, $match3, PREG_OFFSET_CAPTURE))
 	{
-		$n = explode($wanted[4], $s);
-		$dict[$wanted[4]] = $n[1];
-		$s = $n[0];
+		array_push($siralanacak,$match3[0][1]);	
 	}
-	if (preg_match("/Uyarılar/", $s))
+	if (preg_match("/Uyarılar/", $s, $match4, PREG_OFFSET_CAPTURE))
 	{
-		$p = explode($wanted[3], $s);
-		$dict[$wanted[3]] = $p[1];
-		$s = $p[0];
+		array_push($siralanacak,$match4[0][1]);	
 	}
-	if (preg_match("/Kontra Endikasyon/", $s))
+	if (preg_match("/Kontra Endikasyon/", $s, $match5, PREG_OFFSET_CAPTURE))
 	{
-		$r = explode($wanted[2], $s);
-		$dict[$wanted[2]] = $r[1];
-		$s = $r[0];
+		array_push($siralanacak,$match5[0][1]);	
 	}
-	if (preg_match("/Ambalaj/", $s))
+	if (preg_match("/Endikasyon/", $s , $match6, PREG_OFFSET_CAPTURE))
 	{
-		$j = explode($wanted[1], $s);
-		$dict[$wanted[1]] = $j[1];
-		$s = $j[0];
+		array_push($siralanacak,$match6[0][1]);
 	}
-	return $dict;
+	if (preg_match("/Ambalaj/", $s , $match7, PREG_OFFSET_CAPTURE))
+	{
+		array_push($siralanacak,$match7[0][1]);
+	}
+	sort($siralanacak);
+	
+	yazdir($s,$siralanacak[0],$siralanacak[1]-1);
+	yazdir($s,$siralanacak[1],$siralanacak[2]-1);
+	yazdir($s,$siralanacak[2],$siralanacak[3]-1);
+	yazdir($s,$siralanacak[3],$siralanacak[4]-1);
+	yazdir($s,$siralanacak[4],$siralanacak[5]-1);
+	yazdir($s,$siralanacak[5],$siralanacak[6]-1);
+	yazdir($s,$siralanacak[6],strlen($s)-1);
 }
-
+function yazdir($s, $basla, $bitis)
+{
+	for ($i = $basla; $i <= $bitis; $i += 1)
+		print_r( $s[$i]);		
+}
 ?>
