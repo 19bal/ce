c1a4753361d9230bf450d752969949b9e1f217ef
yunus <yunus.ates@bil.omu.edu.tr>
2011-12-13 01:09:10 +0200

    Update inc/content_split.php

--- inc/content_split.php
+++ inc/content_split.php
@@ -9,45 +9,43 @@ function content_split($s) {
 			'Doz Önerisi:');
 
 	$dict = array();
-	$siralanacak = array();
+	$dizi = array();
 
 	if (preg_match("/Doz Önerisi/", $s ,$match1, PREG_OFFSET_CAPTURE))
 	{
-		array_push($siralanacak,$match1[0][1]);		
+		array_push($dizi,$match1[0][1]);		
 	}
 	if (preg_match("/İlaç Etkileşimleri/", $s, $match2, PREG_OFFSET_CAPTURE))
 	{
-		array_push($siralanacak, $match2[0][1]);
+		array_push($dizi, $match2[0][1]);
 	}
 	if (preg_match("/Yan Etkileri/", $s, $match3, PREG_OFFSET_CAPTURE))
 	{
-		array_push($siralanacak,$match3[0][1]);	
+		array_push($dizi,$match3[0][1]);	
 	}
 	if (preg_match("/Uyarılar/", $s, $match4, PREG_OFFSET_CAPTURE))
 	{
-		array_push($siralanacak,$match4[0][1]);	
+		array_push($dizi,$match4[0][1]);	
 	}
 	if (preg_match("/Kontra Endikasyon/", $s, $match5, PREG_OFFSET_CAPTURE))
 	{
-		array_push($siralanacak,$match5[0][1]);	
+		array_push($dizi,$match5[0][1]);	
 	}
 	if (preg_match("/Endikasyon/", $s , $match6, PREG_OFFSET_CAPTURE))
 	{
-		array_push($siralanacak,$match6[0][1]);
+		array_push($dizi,$match6[0][1]);
 	}
 	if (preg_match("/Ambalaj/", $s , $match7, PREG_OFFSET_CAPTURE))
 	{
-		array_push($siralanacak,$match7[0][1]);
+		array_push($dizi,$match7[0][1]);
 	}
-	$sirali = sort($siralanacak);
+	sort($dizi);
 	
-	$dict[$wanted[0]] = substr($s, $sirali[0],$sirali[1] - $sirali[0]);
-	for ( $i = 0; $i < count($sirali); $i += 1){
-		if( $i != count($sirali) - 1)
-			$dict[$wanted[$i]]= substr($s, $sirali[$i],$sirali[$i + 1] - $sirali[$i]);
-		else
-			$dict[$wanted[$i]]= substr($s, $sirali[$i]);
-		}
+	for( $i = 0; $i < count($dizi) - 1; $i += 1)
+		$dict[$wanted[$i]]= substr($s, $dizi[$i],$dizi[$i +1] - $dizi[$i]);
+	
+	$dict[$wanted[count($dizi) - 1]]= substr($s, $dizi[count($dizi) - 1]);
+
 	
 	return $dict;
 }
