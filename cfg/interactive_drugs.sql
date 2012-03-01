3273baf2bcc3c0dc632e1feaf99d592519610310
gdemir <gdemir@bil.omu.edu.tr>
2012-03-01 11:15:09 +0200

    hmm

---
+++ cfg/interactive_drugs.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS `interactive_drugs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `interactive_drugs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) COLLATE utf8_turkish_ci NOT NULL,
+  `content` text COLLATE utf8_turkish_ci NOT NULL,
+  `rate` tinyint(1) COLLATE utf8_turkish_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=30436 DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
